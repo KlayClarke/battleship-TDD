7711a94f6dc988c40eb2fa475c91eefc5f83b19b
Klay Clarke <klayaclarke@gmail.com>
2022-02-21 00:31:04 -0500

    add check user ship position function that makes sure a users ship is placed in legal spot (return true if so) - add randomize cpu position function that randomizes cpu ship on gameboard (makes legal moves)

--- src/script.js
+++ src/script.js
@@ -1,186 +1,3 @@
-const gameBoard = (l, w) => {
-  const board = {};
-
-  board.length = l;
-  board.width = w;
-
-  board.dimensions = [board.length, board.width];
-
-  board.ships = [];
-
-  board.missedAttackCoords = [];
-
-  board.addShip = function (ship) {
-    this.ships.push([ship, ship.topLeftCornerCoord]);
-  };
-
-  board.checkUserShipPosition = function () {
-    let userShip = this.ships[1][0];
-    let userShipPosition = userShip.initalPosition;
-    return userShipPosition;
-
-    let shipOneLength = shipOne[0]["length"];
-    let shipOneWidth = shipOne[0]["width"];
-    let shipTwoLength = shipTwo[0]["length"];
-    let shipTwoWidth = shipTwo[0]["width"];
-    let shipOneTopLeftCornerX = shipOne[0]["topLeftCornerCoord"][0];
-    let shipOneTopLeftCornerY = shipOne[0]["topLeftCornerCoord"][1];
-    let shipTwoTopLeftCornerX = shipTwo[0]["topLeftCornerCoord"][0];
-    let shipTwoTopLeftCornerY = shipTwo[0]["topLeftCornerCoord"][1];
-
-    // check shipOne x coord: must be between 1 and board length - (ship length - 1)
-    if (
-      !(
-        shipOneTopLeftCornerX >= 1 &&
-        shipOneTopLeftCornerX <= this.length - (shipOneLength - 1)
-      )
-    ) {
-      console.log("Ship One X");
-      return false;
-    }
-
-    // check shipOne y coord: must be between 1 and board width / 2 - (ship width - 1)
-    if (
-      !(
-        shipOneTopLeftCornerY >= 1 &&
-        shipOneTopLeftCornerY <= this.width / 2 - (shipOneWidth - 1)
-      )
-    ) {
-      console.log("Ship One Y");
-      return false;
-    }
-
-    // check shiptwo x coord - between 1 and board length - (ship length - 1)
-    if (
-      !(
-        shipTwoTopLeftCornerX >= 1 &&
-        shipTwoTopLeftCornerX <= this.length - (shipTwoLength - 1)
-      )
-    ) {
-      console.log("Ship Two X");
-      return false;
-    }
-
-    // check shiptwo y coord - between (board width / 2) + 1 and board width - (ship width - 1)
-    if (
-      !(
-        shipTwoTopLeftCornerY > this.width / 2 &&
-        shipTwoTopLeftCornerY <= this.width - (shipTwoWidth - 1)
-      )
-    ) {
-      console.log("Ship Two Y");
-      return false;
-    }
-
-    return true;
-  };
-
-  board.recieveAttack = function (attackedShip, attackedCoords) {
-    attackedShip.positions.contains(attackedCoords)
-      ? attackedShip.hit(attackedCoords)
-      : this.missedAttackCoords.push(attackedCoords);
-  };
-
-  board.changeDimensions = function (newLength, newWidth) {
-    board.length = newLength;
-    board.width = newWidth;
-    board.dimensions = [board.length, board.width];
-  };
-
-  return board;
-};
-
-const shipFactory = (length, width, position) => {
-  const ship = {};
-
-  ship.length = length;
-  ship.width = width;
-
-  ship.dimensions = [ship.length, ship.width];
-
-  // the initial placement of ship - more specifically, where the ship's top left corner is placed
-  ship.initalPosition = position;
-
-  // a list of each grid that contains a piece of this ship
-  ship.allPositions = [];
-
-  // a list of all positions hit
-  ship.positionsHit = [];
-
-  // takes a number and adds ship to board with ship top left corner being placed at the grid item with num id
-  ship.addToBoard = function (num) {
-    this.topLeftCornerCoord = num;
-  };
-
-  // every time a ship's position is hit, decrease length by one and log position
-  ship.hit = function (pos) {
-    this.positions.includes(pos) ? this.positionsHit.push(pos) : null;
-    this.positions.filter((position) => position != pos);
-  };
-
-  // if ship has no positions left, it has sunk - return true, else false
-  ship.isSunk = function () {
-    return this.allPositions.length == 0 ? true : false;
-  };
-
-  console.log(ship);
-  return ship;
-};
-
-function checkShipOnePosition(
-  shipOneLength,
-  shipOneWidth,
-  shipOneX,
-  shipOneY,
-  boardLength,
-  boardWidth
-) {
-  // check shipOne x coord: must be between 1 and board length - (ship length - 1)
-  if (!(shipOneX >= 1 && shipOneX <= boardLength - (shipOneLength - 1))) {
-    console.log("Ship One X");
-    return false;
-  }
-
-  // check shipOne y coord: must be between 1 and board width / 2 - (ship width - 1)
-  if (!(shipOneY >= 1 && shipOneY <= boardWidth / 2 - (shipOneWidth - 1))) {
-    console.log("Ship One Y");
-    return false;
-  }
-
-  return true;
-}
-
-function checkShipTwoPosition(
-  shipTwoLength,
-  shipTwoWidth,
-  shipTwoX,
-  shipTwoY,
-  boardLength,
-  boardWidth
-) {
-  // check shiptwo x coord - between 1 and board length - (ship length - 1)
-  if (!(shipTwoX >= 1 && shipTwoX <= boardLength - (shipTwoLength - 1))) {
-    console.log("Ship Two X");
-    return false;
-  }
-
-  // check shiptwo y coord - between (board width / 2) + 1 and board width - (ship width - 1)
-  if (
-    !(shipTwoY > boardWidth / 2 && shipTwoY <= boardWidth - (shipTwoWidth - 1))
-  ) {
-    console.log("Ship Two Y");
-    return false;
-  }
-  return true;
-}
-
-module.exports = {
-  gameBoard,
-  shipFactory,
-  checkShipOnePosition,
-  checkShipTwoPosition,
-};
-
 // create player
 // the game is played against computer - make cpu capable of random plays
 // ai doesn't have to be intelligent, but must know whether a move is legal ('maybe log previously used coords and make sure they are only used once')
@@ -200,4 +17,102 @@ module.exports = {
 
 // for ai, after ai hits user, ai should send weapon to spot near previously hit spot
 
-// don't console log or use DOM to test code, strictly Jest
+const gameBoard = (l, w) => {
+  const board = {};
+
+  board.length = l;
+  board.width = w;
+
+  board.dimensions = [board.length, board.width];
+
+  board.ships = [];
+
+  board.missedAttackCoords = [];
+
+  board.addShip = function (ship) {
+    this.ships.push(ship);
+  };
+
+  board.checkUserShipPosition = function (
+    shipLength,
+    shipInitialPosition,
+    boardLength,
+    boardWidth
+  ) {
+    return shipInitialPosition > (boardLength * boardWidth) / 2 &&
+      parseInt(shipInitialPosition.toString()[1]) <=
+        boardLength - (shipLength - 1)
+      ? true
+      : false;
+  };
+
+  board.recieveAttack = function (attackedShip, attackedCoords) {
+    attackedShip.positions.contains(attackedCoords)
+      ? attackedShip.hit(attackedCoords)
+      : this.missedAttackCoords.push(attackedCoords);
+  };
+
+  return board;
+};
+
+const shipFactory = (length, width) => {
+  const ship = {};
+
+  ship.length = length;
+  ship.width = width;
+
+  ship.dimensions = [ship.length, ship.width];
+
+  ship.initalPosition = 0;
+
+  // a list of each grid that contains a piece of this ship
+  ship.allPositions = [];
+
+  // a list of all positions hit
+  ship.positionsHit = [];
+
+  // the initial placement of ship - more specifically, where the ship's top left corner is placed
+  ship.setInitialPosition = function (pos) {
+    this.initalPosition = pos;
+  };
+
+  // every time a ship's position is hit, decrease length by one and log position
+  ship.hit = function (pos) {
+    this.positions.includes(pos) ? this.positionsHit.push(pos) : null;
+    this.positions.filter((position) => position != pos);
+  };
+
+  // if ship has no positions left, it has sunk - return true, else false
+  ship.isSunk = function () {
+    return this.allPositions.length == 0 ? true : false;
+  };
+
+  ship.randomizeCPUShipPosition = function (
+    shipLength,
+    boardLength,
+    boardWidth
+  ) {
+    let max = (boardLength * boardWidth) / 2;
+    let randomInt = Math.floor(Math.random() * max) + 1;
+
+    // algo gets random numbers until they meet the criteria below, then returns current random num as cpu randomized pos
+    while (
+      !(
+        randomInt > 0 &&
+        randomInt <= max &&
+        (randomInt.toString().length == 1 ||
+          parseInt(randomInt.toString()[1]) <= boardLength - (shipLength - 1))
+      )
+    ) {
+      randomInt = Math.floor(Math.random() * max) + 1;
+    }
+    return randomInt;
+  };
+
+  return ship;
+};
+
+module.exports = {
+  gameBoard,
+  shipFactory,
+};
